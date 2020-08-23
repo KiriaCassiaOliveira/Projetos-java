eb85615c955fadf5bb094c68997b5b83b393a1e5
KiriaCassiaOliveira <kiriacsoliveira@gmail.com>
2020-08-22 23:23:36 -0300

    projeto_banco

---
+++ src/cep/test.java
@@ -0,0 +1,28 @@
+package cep;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/kiria")
+public class test extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter writer = resp.getWriter();
+		
+		writer.write("{" 
+		+ "nome: Kíria," 
+		+ "idade: 22," 
+		+ "sexo: Feminino"
+		+ "}");
+		
+	}
+}
